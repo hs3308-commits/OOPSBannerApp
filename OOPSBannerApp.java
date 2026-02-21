72f4dd9a6690ce96d2ded3cb18761ddcd17655fe
shreya singh <hs3308@srmist.edu.in>
2026-02-21 13:32:41 +0530

    Refactor OOPSBannerApp to print a bannerwithstringfunction
    Refactor OOPSBannerApp class and add banner printing functionality.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,21 @@
-public class OOPSBannerApp {
+class OOPSBannerApp {
 	public static void main(String[] args) {
-		System.out.println("OOPS");
-	}
+
+        String[] banner = {
+			String.join(" ","   ***        ***      ******        ***** "),
+            String.join(" "," **   **    **   **    **    **     **     "),
+            String.join(" ","**     **  **     **   **     **   **     "),
+            String.join(" ","**     **  **     **   **    **     **  "),
+            String.join(" ","**     **  **     **   ******        ***"),
+            String.join(" ","**     **  **     **   **              **  "),
+            String.join(" ","**     **  **     **   **               **     "),
+            String.join(" "," **   **    **    **   **              **       "),
+            String.join(" ","   ***        ***      **         *****   ") 
+        };
+
+        
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
 }
